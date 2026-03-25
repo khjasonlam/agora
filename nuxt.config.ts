811c8a9e76ac3d1addc058717e06cd95bb6a3ff0
khjasonlam <khjasonlam@gmail.com>
2026-03-26 06:15:03 +0900

    fix: correct formatting in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ export default defineNuxtConfig({
     '@nuxt/ui',
     '@nuxtjs/supabase',
     '@pinia/nuxt',
-    '@vercel/speed-insights'
+    '@vercel/speed-insights',
     '@vercel/analytics'
   ],
 
