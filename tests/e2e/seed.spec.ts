c5050302cf3e2addcee450c20f066498c8eeec7c
khjasonlam <khjasonlam@gmail.com>
2026-03-27 02:35:19 +0900

    fix: simplify seed test structure in e2e tests

--- tests/e2e/seed.spec.ts
+++ tests/e2e/seed.spec.ts
@@ -1,8 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { test, expect } from '@playwright/test'
 
-test.describe('Test group', () => {
-  test('seed', async ({ page }) => {
-    // generate code here.
-  })
+test('seed', async ({ page }) => {
+  // generate code here.
 })
